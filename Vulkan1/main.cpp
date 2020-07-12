de364f528c83997d279ca714657f315badfaf53d
sfriss <sfriss@1904labs.com>
2020-07-12 09:47:50 -0500

    Added GLFW window and Vulkan init

--- Vulkan1/main.cpp
+++ Vulkan1/main.cpp
@@ -9,32 +9,82 @@
 //https://vulkan-tutorial.com/Development_environment
 
 
-/*
-#include <vulkan/vulkan.h>
+
+//Automatically loads the vulkan header #include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan() {
-
+    GLFWwindow* window;
+    VkInstance instance;
+    
+    void initWindow() {
+        glfwInit();
+        //This tells GLFW not to use OpenGL, since it was designed to create an OpenGL context
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        
+        //Resizing windows takes special care so disable for now
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        
+        //Params are width, height, title, and which monitor to open on.  The last is OpenGL specific so we can ignore
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
-
+    
+    void initVulkan() {
+        createInstance();
+    }
+    
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        
+        createInfo.enabledLayerCount = 0;
+        
+         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+             throw std::runtime_error("failed to create instance!");
+         }
+    }
+    
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 };
 
@@ -50,10 +100,10 @@ int main() {
 
     return EXIT_SUCCESS;
 }
-*/
 
 
 
+/*
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -90,4 +140,4 @@ int main() {
     return 0;
 }
 
-
+*/
